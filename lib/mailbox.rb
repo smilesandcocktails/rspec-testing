eceda854b8a1dd5f93ba19279802db5f7a736f18
Cara Chew <carachew@gmail.com>
2017-04-25 00:02:57 +0800

    up to height of tree

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,13 +1,23 @@
 class MailBox
+
+  attr_accessor :letters
+  attr_accessor :packages
+
   def initialize(letters, packages)
+    @letters = letters
+    @packages = packages
   end
 
   def add_letter
+    @letters += 1
   end
 
   def add_package
+    @packages += 1
   end
 
   def clear
+    @letters = 0
+    @packages = 0
   end
 end
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,16 @@
 class Tree
+
+  attr_accessor :type
+  attr_accessor :height
+  attr_accessor :age
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    @height = (1/10) * water_amount
   end
 end
